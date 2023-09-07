017b35a01f69501e6dea869dc963089d994c2017
Tom Noble <t.s.noble@outlook.com>
2023-09-07 13:11:26 +0100

    Adds index.html to key bucket

--- aptly_s3_poc/aws_s3_deployment.py
+++ aptly_s3_poc/aws_s3_deployment.py
@@ -10,7 +10,7 @@ class SingleFileBucketDeployment(s3_deploy.BucketDeployment):
     def __init__(self, file: Path, *args, **kwargs):
         file_asset = s3_deploy.Source.asset(
             path=str(file.parent),
-            exclude=["**", f"!{file.name}"],
+            exclude=["**", ".*", f"!{file.name}"],
         )
         super(SingleFileBucketDeployment, self).__init__(
             *args, **kwargs, sources=[file_asset],
