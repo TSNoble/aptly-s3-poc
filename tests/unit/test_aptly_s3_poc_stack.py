c998103949a6497653f695b676a3bb139f65794c
Tom Noble <t.s.noble@outlook.com>
2023-09-12 08:05:49 +0100

    Renames files and directories

--- tests/unit/test_aptly_s3_poc_stack.py
+++ tests/unit/test_aptly_s3_poc_stack.py
@@ -1,13 +1,13 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from aws.stacks.aptly_repository_stack import AptlyS3PocStack
+from aws.stacks.aptly_repository_stack import AptlyRepositoryStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in aptly_s3_poc/aptly_s3_poc_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = AptlyS3PocStack(app, "aptly-s3-poc")
+    stack = AptlyRepositoryStack(app, "aptly-s3-poc")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
