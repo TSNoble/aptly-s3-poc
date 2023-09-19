18629d63f42b4d62ef6f33d2ae4a22ed65f57785
Tom Noble <53005340+TSNoble@users.noreply.github.com>
2023-09-19 10:44:23 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ from aws.stacks.aptly_repository_stack import AptlyRepositoryStack
 
 app = cdk.App()
 
+github_branch = os.getenv("GITHUB_REF", default="")
 github_provider_arn = os.environ["AWS_GITHUB_PROVIDER_ARN"]
 
 AptlyRepositoryStack(
     scope=app,
-    id="AptlyRepositoryStack",
+    id=f"{github_branch}AptlyRepositoryStack",
     github_provider_arn=github_provider_arn,
     env=cdk.Environment(account="778015471639", region="us-east-1"),
 )
