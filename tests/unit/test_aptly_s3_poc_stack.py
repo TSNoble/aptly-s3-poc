657015387a57c63998f503da565cf80f660e2e2d
Tom Noble <tom@rivelinrobotics.com>
2023-09-20 14:46:07 +0100

    Adds debug prints

--- tests/unit/test_aptly_s3_poc_stack.py
+++ tests/unit/test_aptly_s3_poc_stack.py
@@ -8,5 +8,9 @@ def test_unauthenticated_user_cannot_access_public_key():
     session = boto3.Session(aws_access_key_id="foo", aws_secret_access_key="bar")
     s3 = session.client("s3")
     bucket = os.environ["AWS_APTLY_BUCKET_NAME"]
-    with pytest.raises(s3.exceptions.NoSuchKey):
+    try:
         s3.get_object(Bucket=bucket, Key="public.pgp")
+        pytest.fail("Should not be able to access public key")
+    except s3.exceptions.ClientError as e:
+        print(e)
+        assert e.error_code == "AccessDenied"
