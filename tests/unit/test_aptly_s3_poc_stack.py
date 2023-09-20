c3516b5c331a60484f8269df6b9baf4015ef9bb4
Tom Noble <tom@rivelinrobotics.com>
2023-09-20 14:23:03 +0100

    Adds test

--- tests/unit/test_aptly_s3_poc_stack.py
+++ tests/unit/test_aptly_s3_poc_stack.py
@@ -1,17 +1,11 @@
-import aws_cdk as core
-import aws_cdk.assertions as assertions
+import os
 
-from aws.stacks.aptly_repository_stack import AptlyRepositoryStack
-
-
-# example tests. To run these tests, uncomment this file along with the example
-# resource in aptly_s3_poc/aptly_s3_poc_stack.py
-def test_sqs_queue_created():
-    app = core.App()
-    stack = AptlyRepositoryStack(app, "aptly-s3-poc")
-    template = assertions.Template.from_stack(stack)
+import pytest
+import boto3
 
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+def test_unauthenticated_user_cannot_access_public_key():
+    s3 = boto3.client("s3")
+    bucket = os.environ["AWS_APTLY_BUCKET_NAME"]
+    with pytest.raises(s3.exceptions.NoSuchKey):
+        s3.get_object(Bucket=bucket, Key="public.pgp")
