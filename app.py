31ba107c7099d2956fedeaddb06c26491c5f40e8
Tom Noble <tom@rivelinrobotics.com>
2023-09-25 10:25:47 +0100

    Adds debug prints

--- app.py
+++ app.py
@@ -11,20 +11,20 @@ from aws.stacks import (
 
 app = cdk.App()
 
-github_branch = os.getenv("GITHUB_HEAD_REF", default="")
+stack_prefix = os.getenv("STACK_PREFIX", default="")
 github_provider_arn = os.environ["AWS_GITHUB_PROVIDER_ARN"]
 us_east_dev_account = env = cdk.Environment(account="778015471639", region="us-east-1")
 
 repository = repository_stack.AptlyRepositoryStack(
     scope=app,
-    id=f"{github_branch}AptlyRepositoryStack",
+    id=f"{stack_prefix}AptlyRepositoryStack",
     github_provider_arn=github_provider_arn,
     env=us_east_dev_account,
 )
 
 domain = domain_stack.AptlyDomainStack(
     scope=app,
-    id=f"{github_branch}AptlyDomainStack",
+    id=f"{stack_prefix}AptlyDomainStack",
     aptly_repository_stack=repository,
     domain="dev.downloads.rivel.in",
     env=us_east_dev_account,
